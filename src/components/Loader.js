4979fab6575c5979006fdfea061f4f47d2eee014
kuldeeepkumar <pro.kuldeepkumar@gmail.com>
2024-06-27 16:16:24 +0530

    Activity Indicator component added and paper packages installed , some folder sturcture corrections are done

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import * as React from "react";
+
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
+
+import { COLORS } from "../constants/Colors";
+import { Text } from "react-native";
+import { View } from "react-native";
+import { containerStyle } from "../styles";
 
 const Loader = () => {
   return (
-    <View>
-      <Text>Loader</Text>
-    </View>
-  )
-}
+    <>
+      <View style={containerStyle.container}>
+        <ActivityIndicator
+          size={"large"}
+          animating={true}
+          theme={{ colors: { primary: COLORS.INDICATOR_COLOR } }}
+        />
+        <Text
+          style={{
+            color: COLORS.LABEL_COLOR,
+            marginTop: 20,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}>
+          Loading...
+        </Text>
+      </View>
+    </>
+  );
+};
 
-export default Loader
-
-const styles = StyleSheet.create({})
+export default Loader;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,46 +1,53 @@
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
-import CustomButton from "./components/CustomButton";
-
-import React from "react";
+import CustomButton from "../components/customButton";
+import Loader from "../components/Loader";
 import { containerStyle } from "../styles";
 
 const WelcomeImage = require("../../assets/img/welcome.jpg");
 
 const Welcome = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <ImageBackground
-      imageStyle={{ opacity: 0.7 }}
-      source={WelcomeImage}    
-      style={[containerStyle.container, {}]}>
-      <Text
-        style={{
-          fontSize: 100,
-          fontWeight: "bold",
-          color: "black",
-          fontFamily: "Inter-Black",
-          backgroundColor: "yellow",
-          paddingHorizontal: 10,
-        }}>
-        FLIGHT
-      </Text>
-      <Text
-        style={{
-          fontSize: 45,
-          fontWeight: "bold",
-          color: "black",
-          fontFamily: "Inter-Black",
-          borderColor: "yellow",
-          borderWidth: 2,
-          padding: 12,
-        }}>
-        TRACKER APP
-      </Text>
+    <>
+      {isLoading === false ? (
+        <ImageBackground
+          imageStyle={{ opacity: 0.9 }}
+          source={WelcomeImage}
+          style={[containerStyle.container, {}]}>
+          <Text
+            style={{
+              fontSize: 100,
+              fontWeight: "bold",
+              color: "black",
+              fontFamily: "Inter-Black",
+              backgroundColor: "yellow",
+              paddingHorizontal: 10,
+            }}>
+            FLIGHT
+          </Text>
+          <Text
+            style={{
+              fontSize: 45,
+              fontWeight: "bold",
+              color: "black",
+              fontFamily: "Inter-Black",
+              borderColor: "yellow",
+              borderWidth: 2,
+              padding: 12,
+            }}>
+            TRACKER APP
+          </Text>
 
-      <View style={{ marginTop: 200 }}>
-        <CustomButton title={"Get Started"}></CustomButton>
-      </View>
-    </ImageBackground>
+          <View style={{ marginTop: 200 }}>
+            <CustomButton title={"Get Started"}></CustomButton>
+          </View>
+        </ImageBackground>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
